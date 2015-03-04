739afa8b7b7bc654934c53dd979f10a6332dc6c0
newellworldorder <bwongggg@gmail.com>
2015-03-03 19:52:00 -0500

    Fixed unkick/unban

--- op.py
+++ op.py
@@ -1,88 +1,85 @@
 import hexchat as hexchat
+import time
 
 __module_name__ = 'Hidden Op'
 __module_author__ = 'NewellWorldOrder'
 __module_version__ = '1'
 __module_description__ = 'Stealth op commands'
 
-bannedUser = ''
-
 def op_make(word, word_eol, userdata):
     chan = hexchat.get_info('channel')
     if len(word) < 2:
-        hexchat.command('cs op ' + chan)
+        hexchat.command('cs op %s' % chan)
     else:
-        hexchat.command('cs op ' + chan + ' ' + word_eol[1])
+        hexchat.command('cs op {} {}'.format(chan, word_eol[1]))
     return hexchat.EAT_ALL
 
 def op_remove(word, word_eol, userdata):
     chan = hexchat.get_info('channel')
     if len(word) < 2:
-        hexchat.command('cs deop ' + chan)
+        hexchat.command('cs deop %s' % chan)
     else:
-        hexchat.command('cs deop ' + chan + ' ' + word_eol[1])
+        hexchat.command('cs deop {} {}'.format(chan, word_eol[1]))
     return hexchat.EAT_ALL
 
 def op_kick(word, word_eol, userdata):
     users = hexchat.get_list('users')
     chan = hexchat.get_info('channel')
-    if len(word) > 0:
-        for user in users:
-            if user.nick.lower() == word[1].lower():
-                break
-        bannedUser = '*!*@%s' % user.host.split('@')[1]
-        hexchat.command('cs op ' + chan)
-        hexchat.command('timer 1 kickban ' + word_eol[1])
-        hexchat.command('timer 1 cs deop ' + chan)
+    if len(word) > 1:
+        hexchat.command('cs op %s' % chan)
+        hexchat.command('timer 1 kickban %s' % word_eol[1])
+        hexchat.command('timer 1 cs deop %s' % chan)
 
     return hexchat.EAT_ALL
 
 def op_whois(word, word_eol, userdata):
-    if len (word) > 0:
-        print(word[2])
-        
-    return hexchat.EAT_ALL
-
-def op_unban(word, word_eol, userdata):
     chan = hexchat.get_info('channel')
-    hexchat.command('whois %s' % word[1])
-    hexchat.command('cs op ' + chan)
-    hexchat.command('cs deop ' + chan)
-            
+    bUser = word[2]
+    hexchat.command('cs op %s' % chan)
+    hexchat.command('timer 1 unban *!*@%s' % bUser)
+    hexchat.command('invite %s' % userdata)
+    hexchat.command('timer 1 cs deop %s' % chan)
+    
+    return hexchat.EAT_NONE
+
+def op_unkick(word, word_eol, userdata):
+    if len(word) > 1:
+        hexchat.command('whois %s' % word[1])
+        hexchat.hook_print('WhoIs Name Line', op_whois, word[1])
+    
     return hexchat.EAT_ALL
 
 def op_quiet(word, word_eol, userdata):
     users = hexchat.get_list('users')
     chan = hexchat.get_info('channel')
-    if len(word) > 0:
+    if len(word) > 1:
         for user in users:
             if user.nick.lower() == word[1].lower():
                 break
         
-        hexchat.command('cs op ' + chan)
-        hexchat.command('mode %s +q *!*@%s' % (chan, user.host.split('@')[1]))
-        hexchat.command('cs deop ' + chan)
+        hexchat.command('cs op %s' % chan)
+        hexchat.command('mode {} +q *!*@{}'.format(chan, user.host.split('@')[1]))
+        hexchat.command('cs deop %s' % chan)
             
     return hexchat.EAT_ALL
 
 def op_unquiet(word, word_eol, userdata):
     users = hexchat.get_list('users')
     chan = hexchat.get_info('channel')
-    if len(word) > 0:
+    if len(word) > 1:
         for user in users:
             if user.nick.lower() == word[1].lower():
                 break
         
-        hexchat.command('cs op ' + chan)
-        hexchat.command('mode %s -q *!*@%s' % (chan, user.host.split('@')[1]))
-        hexchat.command('cs deop ' + chan)
+        hexchat.command('cs op %s' % chan)
+        hexchat.command('mode {} -q *!*@{}'.format(chan, user.host.split('@')[1]))
+        hexchat.command('cs deop %s' % chan)
             
     return hexchat.EAT_ALL
 
 hexchat.hook_command('op', op_make)
 hexchat.hook_command('deop', op_remove)
 hexchat.hook_command('kick', op_kick)
-hexchat.hook_print('Whois Real Host', op_whois)
-hexchat.hook_command('unban', op_unban)
+hexchat.hook_command('unkick', op_unkick)
 hexchat.hook_command('quiet', op_quiet)
 hexchat.hook_command('unquiet', op_unquiet)
