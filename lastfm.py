5d8d911f0e166ba5821e16d6c1f6510039a64d82
newellworldorder <bwongggg@gmail.com>
2015-03-10 18:06:31 -0400

    lastfmpls

--- lastfm.py
+++ lastfm.py
@@ -6,10 +6,10 @@ __module_author__ = 'NewellWorldOrder'
 __module_version__ = '1'
 __module_description__ = 'Announces current song playing in Foobar for Windows'
 
-API_KEY = "API_KEY"
-API_SECRET = "API_SECRET_KEY"
-lastfm_username = "username"
-lastfm_password_hash = "md5hashofpassword"
+API_KEY = "7708894264808d07c54730658c42b6e5"
+API_SECRET = "a7916004c04083ca31d97fdc1e2e9255"
+lastfm_username = "brnwng"
+lastfm_password_hash = "0366c0f551dc1e5a2854731822c62b71"
 lastfm_network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET, username=lastfm_username, password_hash=lastfm_password_hash)
 
 def np(word, word_eol, userdata):
