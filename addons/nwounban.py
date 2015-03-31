f97617355b4ede48d167b61879f5d2a2d2ed083f
newellworldorder <bwongggg@gmail.com>
2015-03-31 18:18:08 -0400

    Revert "sjfgo;iajwfdoi"
    This reverts commit cfafbff3c0e1b1f8873d245a51751cea9531806e.

--- addons/nwounban.py
+++
@@ -1,8 +0,0 @@
-import hexchat
-
-__module_name__ = 'NWOunban'
-__module_author__ = 'NewellWorldOrder'
-__module_version__ = '1.0'
-__module_description__ = 'Auto unban and rejoin script'
-
-hexchat.hook_server("474", channel)
