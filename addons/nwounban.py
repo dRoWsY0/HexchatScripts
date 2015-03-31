cfafbff3c0e1b1f8873d245a51751cea9531806e
newellworldorder <bwongggg@gmail.com>
2015-03-31 18:18:03 -0400

    sjfgo;iajwfdoi
    EAT_PLUGIN

---
+++ addons/nwounban.py
@@ -0,0 +1,8 @@
+import hexchat
+
+__module_name__ = 'NWOunban'
+__module_author__ = 'NewellWorldOrder'
+__module_version__ = '1.0'
+__module_description__ = 'Auto unban and rejoin script'
+
+hexchat.hook_server("474", channel)
