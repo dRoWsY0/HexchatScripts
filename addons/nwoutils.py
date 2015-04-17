df27c55bc9b327ffeda90b77740daba4875adcd7
newellworldorder <bwongggg@gmail.com>
2015-04-16 23:33:34 -0400

    asdfasdf

--- addons/nwoutils.py
+++ addons/nwoutils.py
@@ -47,13 +47,27 @@ def nwo_kick(word, word_eol, userdata):
                 break
     return hexchat.EAT_ALL
 
+def nwo_unquiet(word, word_eol, userdata):
+    users = hexchat.get_list('users')
+    chan = hexchat.get_info('channel')
+    nick = hexchat.get_info('nick')
+    if len(word) >= 2:
+        for user in users:
+            if hexchat.nickcmp(user.nick, word[1]) == 0:
+                host = user.host.split('@')[1]
+                hexchat.command('RAW PRIVMSG ChanServ :op %s' % chan)
+                hexchat.command('timer 1 RAW MODE %s -q *!*@%s *!*@%s' % (chan, host, host))
+                hexchat.command('timer 2 RAW MODE %s -o %s' % (chan, nick))
+                break
+    return hexchat.EAT_ALL
+
 def nwo_judo(word, word_eol, userdata):
     chan = hexchat.get_info('channel')
     nick = hexchat.get_info('nick')
     try:
         hexchat.command('RAW PRIVMSG ChanServ :op %s' % chan)
-        hexchat.command('timer 1 RAW %s' % word_eol[1])
-        hexchat.command('timer 2 RAW MODE %s -o *%s' % (chan, nick))
+        hexchat.command('timer 1 %s' % word_eol[1])
+        hexchat.command('timer 2 RAW MODE %s -o %s' % (chan, nick))
     except:
         print('Failed')
     return hexchat.EAT_ALL
@@ -65,7 +79,8 @@ def nwo_dankyamyams(word, word_eol, userdata):
 
 hexchat.hook_command('op', nwo_op, help='/op will op you if +o flags are set on you. /op <nickname> [<nickname>] will op others')
 hexchat.hook_command('deop', nwo_deop, help='/deop will deop you if +o flags are set for you. /deop <nickname> [<nickname>] will deop others')
-hexchat.hook_command('kick', nwo_kick, help='/kick <nickname> will temporarily op you, kickban <nickname> and deop you')
+hexchat.hook_command('kb', nwo_kick, help='/kb <nickname> will temporarily op you, kickban <nickname> and deop you')
+hexchat.hook_command('uq', nwo_unquiet)
 hexchat.hook_command('judo', nwo_judo, help='/judo <op command> temporarily gives you operator status to execute a command.')
 hexchat.hook_command('>', nwo_dankyamyams)
 
